5b83594e966f767f7775d324c28f5ef561442212
anas2002 <anas4112002@gmail.com>
2023-07-14 03:44:55 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    home: Scaffold(
+     appBar: AppBar(title: Text("First App"),),
+     body: Center(child: Text("Hello World")),
+    ),
+  ));
+}
